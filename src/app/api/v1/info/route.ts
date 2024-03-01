92f89a7113606186cd9cb1294e88db17eb2bb174
Alexey Sapon <asapon@freeconferencecall.com>
2024-03-02 01:49:15 +0200

    Updated route function

--- src/app/api/v1/info/route.ts
+++ src/app/api/v1/info/route.ts
@@ -2,7 +2,22 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export const dynamic = 'force-dynamic';
 
-export async function POST(request: NextRequest) {
-	console.log(await request.json());
-	return new Response(null, {status: 200});
+export async function GET(request: NextRequest) {
+	const monores = await fetch('https://api.monobank.ua/personal/client-info', {
+    headers: {
+      'X-Token': process.env.MONO_TOKEN
+    }
+  });
+  const data = await monores.json();
+
+  const account = data.accounts.find(({id}) => id === process.env.MONO_ACCOUNT);
+
+  if (!account) {
+    return NextResponse.error();
+  }
+
+  const {balance, creditLimit} = account;
+  const myOwnBalance = balance - creditLimit;
+
+	return NextResponse.json({balance: myOwnBalance / 100});
 }
