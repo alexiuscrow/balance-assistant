081115efcc6d2b619b6f9ccf6fa26be86fb8136e
Alexey Sapon <asapon@freeconferencecall.com>
2024-06-21 11:36:57 +0300

    Refactor GET function to handle multiple types
    The GET function in main-account balance route has been modified to handle both 'account' and an additional 'type'. Instead of finding and returning details only from 'account', the function now searches over any provided 'type'. Default value is still set to 'account' if no 'type' is specified in the query.

--- src/app/api/v2/main-account/balance/route.ts
+++ src/app/api/v2/main-account/balance/route.ts
@@ -4,7 +4,7 @@ import {NextApiRequest} from 'next';
 export const dynamic = 'force-dynamic';
 
 export async function GET(req: NextApiRequest) {
-  const {account: accountId} = req.query;
+  const {account: accountId, type = 'account'} = req.query;
 
   if (!process.env.MONO_TOKEN || !accountId) {
     return NextResponse.error();
@@ -18,13 +18,13 @@ export async function GET(req: NextApiRequest) {
   const monores = await fetch('https://api.monobank.ua/personal/client-info', {headers});
   const data = await monores.json();
 
-  const account = data.accounts.find(({id}: { id: string }) => id === accountId);
+  const item = data[`${type}s`]?.find(({id}: { id: string }) => id === accountId);
 
-  if (!account) {
+  if (!item) {
     return NextResponse.error();
   }
 
-  const {balance, creditLimit} = account;
+  const {balance, creditLimit} = item;
   const myOwnBalance = balance - creditLimit;
 
   return NextResponse.json({
